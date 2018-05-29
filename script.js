b3140eb86d60ac97a60851c36964221d6c8a58ca
elizabethr25 <emrichardson820@gmail.com>
2018-05-29 16:11:08 -0700

    created hero image added about me and sidebar functionality

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+function open() {
+    document.getElementById("sidebar").style.width = "250px";
+    console.log("clicked");
+}
+
+function close() {
+    document.getElementById("sidebar").style.width = "0";
+    console.log("clicked");
+}
+
+});
