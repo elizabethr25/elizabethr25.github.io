9d32fea07d68720d2d1edcae9d7ef81319524150
elizabethr25 <emrichardson820@gmail.com>
2018-05-30 14:04:26 -0700

    projects and connect pages added with basic styling

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-function open() {
-    document.getElementById("sidebar").style.width = "250px";
-    console.log("clicked");
-}
+// function open() {
+//     document.getElementById("sidebar").style.width = "250px";
+//     console.log("clicked");
+// }
 
-function close() {
-    document.getElementById("sidebar").style.width = "0";
-    console.log("clicked");
-}
+// function close() {
+//     document.getElementById("sidebar").style.width = "0";
+//     console.log("clicked");
+// }
 
-});
+// });
